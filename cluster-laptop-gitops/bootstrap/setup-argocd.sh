2815b0f9a6f2103972850eec4cc6f22598135903
rob passmore <rpassmore@gmail.com>
2022-03-30 10:04:51 +0100

    update argo ngress

--- cluster-laptop-gitops/bootstrap/setup-argocd.sh
+++ cluster-laptop-gitops/bootstrap/setup-argocd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 kubectl create namespace argocd
-kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/core-install.yaml
+kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply -f ./gitops-prj.yaml
-kubectl apply -f ./gitops-app.yaml
+kubectl apply -f ./gitops-cicd-app.yaml
