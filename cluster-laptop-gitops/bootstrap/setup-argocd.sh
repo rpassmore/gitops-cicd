cf7469a1f713b3ecae87a03890ccee818bc073c3
rob passmore <rpassmore@gmail.com>
2022-03-31 10:44:23 +0100

    swap to helm install

--- cluster-laptop-gitops/bootstrap/setup-argocd.sh
+++ cluster-laptop-gitops/bootstrap/setup-argocd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 kubectl create namespace argocd
-kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+#kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+helm repo add argo https://argoproj.github.io/argo-helm
+helm install argocd argo/argo-cd
 kubectl apply -f ./gitops-prj.yaml
 kubectl apply -f ./gitops-cicd-app.yaml
