31c2385621f3cd4e49b1dc7b62603aeb012fab56
rpassmore <rpassmore@gmail.com>
2023-12-16 16:00:27 +0000

    Add home lab gitops

--- cluster-home-lab/bootstrap/setup-argocd.sh
+++ cluster-home-lab/bootstrap/setup-argocd.sh
@@ -2,6 +2,12 @@
 #kubectl create namespace argocd
 #kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 helm repo add argo https://argoproj.github.io/argo-helm
-helm install --namespace argocd --create-namespace argocd argo/argo-cd
+helm repo update
+helm install --namespace argocd --create-namespace argocd --values ./argocd-values.yaml argo/argo-cd 
+#helm upgrade --install --namespace argocd --create-namespace argocd  --values ./argocd-values.yaml argo/argo-cd 
 kubectl apply -f ./gitops-prj.yaml
 kubectl apply -f ./gitops-cicd-app.yaml
+
+
+
+
